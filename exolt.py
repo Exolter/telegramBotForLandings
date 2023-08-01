16c05f07a44c676905917417fb43b7a765b0790d
Exolter <garkusha.company@gmail.com>
2023-08-01 14:47:45 +0300

    first dwnload

---
+++ exolt.py
@@ -0,0 +1,19 @@
+import telebot
+from telebot import types
+
+TOKEN = '5456963743:AAE7su2HddIkmPKaHdC255w_cjK9Z09AkHM'
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard = True)
+    item1 = types.KeyboardButton('Каталог сайтів')
+    item2 = types.KeyboardButton('Допомога')
+    item3 = types.KeyboardButton('Постачальники')
+
+    markup.add(item1, item2, item3)
+
+    bot.send_message(message.chat.id, 'Вітаю, {0.first_name}! В каталозі ви зможете знайти всі доступні на зараз сайти ;)'.format(message.from_user), reply_markup = markup)
+
+bot.polling(non_stop = True)
